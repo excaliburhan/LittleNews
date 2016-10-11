177f464c68b2b6a5ced55734c0153d5c933c502a
xiaoping <edwardhjp@gmail.com>
2016-10-11 17:48:11 +0800

    finish base html

--- js/app.js
+++ js/app.js
@@ -8,9 +8,17 @@
 const $ = require('jquery')
 
 function init() {
+  // prevent drag event
   $('html').on('drop dragover', (e) => {
     e.preventDefault()
     return
+  })
+
+  $('.addBtn').on('click', () => {
+    $('#add').addClass('show')
+  })
+  $('.manageBtn').on('click', () => {
+    $('#manage').addClass('show')
   })
 }
 
