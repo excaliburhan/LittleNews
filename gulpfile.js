0a216acf1b36c22dd6920b3f5d665d0637db8b02
xiaoping <edwardhjp@gmail.com>
2016-10-15 18:34:09 +0800

    finish v0.04 && add yarn

--- gulpfile.js
+++ gulpfile.js
@@ -6,33 +6,15 @@
  */
 
 const gulp = require('gulp')
-const gulpBabel = require('gulp-babel')
-const gulpUglify = require('gulp-uglify')
 const gulpLess = require('gulp-less')
 const cleanCss = require('gulp-clean-css')
-const gulpImagemin = require('gulp-imagemin')
 const concat = require('gulp-concat')
-const del = require('del')
 
 const paths = {
   js: './js/*.js',
   css: './css/*.less',
   img: './img/**',
 }
-
-gulp.task('js', () => {
-  del('js/bundle.min.js')
-  .then(() => {
-    gulp
-      .src(paths.js)
-      .pipe(gulpBabel({
-        presets: ['es2015'],
-      }))
-      .pipe(concat('bundle.min.js'))
-      .pipe(gulpUglify())
-      .pipe(gulp.dest('./js'))
-  })
-})
 
 gulp.task('css', () => {
   gulp
@@ -43,17 +25,8 @@ gulp.task('css', () => {
     .pipe(gulp.dest('./css'))
 })
 
-gulp.task('img', () => {
-  gulp
-    .src(paths.img)
-    .pipe(gulpImagemin())
-    .pipe(gulp.dest('./img'))
-})
-
 gulp.task('reload', () => {
-  // gulp.watch(paths.js, ['js'])
   gulp.watch(paths.css, ['css'])
-  // gulp.watch(paths.img, ['img'])
 })
 
 gulp.task('default', ['reload'])
